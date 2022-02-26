2dc098691ccc9c8522cee463241ad6ed3bdff87d
unknown <daniatitienei@gmail.com>
2022-02-26 12:40:56 +0200

    Register & Login via Google done

---
+++ app/src/main/java/com/goto_delivery/pgoto/ui/utils/authentication/google/GetGoogleSignInClient.kt
@@ -0,0 +1,16 @@
+package com.goto_delivery.pgoto.ui.utils.authentication.google
+
+import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.goto_delivery.pgoto.R
+
+fun getGoogleSignInClient(context: Context): GoogleSignInClient {
+    val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(context.getString(R.string.default_web_client_id))
+        .requestEmail()
+        .build()
+
+    return GoogleSignIn.getClient(context, googleSignInOptions)
+}
